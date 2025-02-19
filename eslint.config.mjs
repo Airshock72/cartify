82bb496e1f9a352326d4fa3b2c47119acde374b6
Airshock72 <gelagela2222@gmail.com>
2025-02-19 18:39:29 +0400

    added eslint prettier ts configs and added apollo client graphql zod and its validator to package json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,39 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      'object-curly-spacing': ['error', 'always'],
+      'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
+      'no-trailing-spaces': 'error',
+      'semi-style': ['error', 'last'],
+      'indent': ['error', 2],
+      'jsx-quotes': ['error', 'prefer-single'],
+      'comma-dangle': [
+        'error',
+        {
+          arrays: 'never',
+          objects: 'never',
+          imports: 'never',
+          exports: 'never',
+          functions: 'never'
+        }
+      ],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'never', { beforeStatementContinuationChars: 'never' }],
+      'no-mixed-spaces-and-tabs': 0
+    }
+  }
+]
 
-export default eslintConfig;
+export default eslintConfig
