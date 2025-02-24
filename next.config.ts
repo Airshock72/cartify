8ae1875fa4732a5223e6303d31467d980ab9855f
Airshock72 <gelagela2222@gmail.com>
2025-02-24 14:56:27 +0400

    rollbacked some extra changes

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  reactStrictMode: false,
-  output: 'export',
-  basePath: '/cartify',
-  assetPrefix: '/cartify/',
-  images: {
-    unoptimized: true
-  },
-  trailingSlash: false
+  reactStrictMode: false
 }
 
 export default nextConfig
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,7 +14,7 @@ class MyDocument extends Document {
           <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' rel='stylesheet'
             integrity='sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH'
             crossOrigin='anonymous'/>
-          <link rel='icon' href='/cartify/favicon.ico'/>
+          <link rel='icon' href='/favicon.ico'/>
         </Head>
         <body>
           <Main/>
